fa1b0af8ec9a71b33e9315262d3cb96c3aae50fc
Mateo Morris <accounts@mateomorris.com>
2023-06-17 11:20:32 -0400

    restore

--- src/lib/Tester.svelte
+++ src/lib/Tester.svelte
@@ -1,68 +1,10 @@
 <script>
-	import { Line } from 'svelte-chartjs';
+	import { SvelteGantt, SvelteGanttTable } from 'svelte-gantt';
+	import Heading from './Heading.svelte';
 
-	import {
-		Chart as ChartJS,
-		Title,
-		Tooltip,
-		Legend,
-		LineElement,
-		LinearScale,
-		PointElement,
-		CategoryScale
-	} from 'chart.js';
-
-	ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale);
+	export let foo = 'EMPTY';
 </script>
 
-<Line
-	data={{
-		labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-		datasets: [
-			{
-				label: 'My First dataset',
-				fill: true,
-				lineTension: 0.3,
-				backgroundColor: 'rgba(225, 204,230, .3)',
-				borderColor: 'rgb(205, 130, 158)',
-				borderCapStyle: 'butt',
-				borderDash: [],
-				borderDashOffset: 0.0,
-				borderJoinStyle: 'miter',
-				pointBorderColor: 'rgb(205, 130,1 58)',
-				pointBackgroundColor: 'rgb(255, 255, 255)',
-				pointBorderWidth: 10,
-				pointHoverRadius: 5,
-				pointHoverBackgroundColor: 'rgb(0, 0, 0)',
-				pointHoverBorderColor: 'rgba(220, 220, 220,1)',
-				pointHoverBorderWidth: 2,
-				pointRadius: 1,
-				pointHitRadius: 10,
-				data: [65, 59, 80, 81, 56, 55, 40]
-			},
-			{
-				label: 'My Second dataset',
-				fill: true,
-				lineTension: 0.3,
-				backgroundColor: 'rgba(184, 185, 210, .3)',
-				borderColor: 'rgb(35, 26, 136)',
-				borderCapStyle: 'butt',
-				borderDash: [],
-				borderDashOffset: 0.0,
-				borderJoinStyle: 'miter',
-				pointBorderColor: 'rgb(35, 26, 136)',
-				pointBackgroundColor: 'rgb(255, 255, 255)',
-				pointBorderWidth: 10,
-				pointHoverRadius: 5,
-				pointHoverBackgroundColor: 'rgb(0, 0, 0)',
-				pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
-				pointHoverBorderWidth: 2,
-				pointRadius: 1,
-				pointHitRadius: 10,
-				data: [28, 48, 40, 19, 86, 27, 90]
-			}
-		]
-	}}
-	width={100}
-	height={50}
-/>
+<Heading />
+<h1>This is the BRAND NEW tester</h1>
+<h2>The prop value is {foo}</h2>
